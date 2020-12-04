347552367def62d3f9ac1f771496b197e60cd806
TSG <TSG@TSGAC>
2020-12-04 15:55:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace The_Collector
+{
+    class Program
+    {
+        public static string Vinyl { get; set; }
+
+        static void Main(string[] args)
+        {
+
+
+            //The Colletor
+            Console.Write("Vinyl release");
+            Vinyl = Console.ReadLine();
+        }
+    }
+}
