67a976726b0c54be651c669f9c60c7bcca442dd5
TSG <TSG@TSGAC>
2020-12-09 16:16:04 -0500

    12.09.2020 Added to the User input section and some refactoring

--- Program.cs
+++ Program.cs
@@ -4,15 +4,30 @@ namespace The_Collector
 {
     class Program
     {
-        public static string Vinyl { get; set; }
+        /* The collector. A catolog of music.
+         As the user buidls or reduces their record colletion, 
+         the user can save, edit, and remove items and save this info to 
+         a document.
+        */
 
         static void Main(string[] args)
         {
 
 
             //The Colletor
-            Console.Write("Vinyl release");
-            Vinyl = Console.ReadLine();
+            Console.Write("Artist name: ");
+            string ArtistName = Console.ReadLine();
+            Console.Write("Artist Location: ");
+            string ArtistLocation = Console.ReadLine();
+            Console.WriteLine("Years Active: ");
+            string YearsAcitve = Console.ReadLine();
+            Console.Write("Number of releases: ");
+            string Releases = Console.ReadLine();
+            Console.Write("Album title: ");
+            string AlbumTitle = Console.ReadLine();
         }
+
+
     }
 }
+  
